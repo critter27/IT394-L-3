805eccb11b58134a418bae111e64e0925b2498b7
critter27 <christopher.ritter@westpoint.edu>
2019-01-23 11:00:36 -0500

    addded the median function

--- trial.py
+++ trial.py
@@ -2,4 +2,10 @@ def aver(nums):
     return sum(nums)/len(nums)
 
 
-print(aver([1,2,3,4,5,6])}
+print(aver([1,2,3,4,5,6]))
+
+
+def medi(nums):
+    return  nums[len(nums)//2]
+
+print(medi([1,2,3,4,5,6]))
